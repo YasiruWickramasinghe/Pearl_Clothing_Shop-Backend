d08698fa2e2445ebb299bc7c895c78a1848df6e5
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2022-04-25 10:41:19 +0530

    pearl-clothing Backend project initialized

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+//routers
+const userRoute = require('./routes/user')
+const authRoute = require('./routes/auth')
+
+dotenv.config()
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('DB Connection Successfull!'))
+  .catch(err => {
+    console.log(err)
+  })
+
+app.use(express.json())
+//rote api call
+app.use('/api/users', userRoute)
+app.use('/api/auth', authRoute)
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Backend server is running!')
+})

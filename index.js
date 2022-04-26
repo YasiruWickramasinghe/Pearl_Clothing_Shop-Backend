ea7a772fd51af7685a623d5f4c8eb330cf77f1dc
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2022-04-26 13:21:00 +0530

    CRUD Complete

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const dotenv = require('dotenv')
 //routers
 const userRoute = require('./routes/user')
 const authRoute = require('./routes/auth')
+const productRoute = require('./routes/product')
+const cartRoute = require('./routes/cart')
+const orderRoute = require('./routes/order')
 
 dotenv.config()
 
@@ -20,6 +23,9 @@ app.use(express.json())
 //rote api call
 app.use('/api/users', userRoute)
 app.use('/api/auth', authRoute)
+app.use('/api/products', productRoute)
+app.use('/api/carts', cartRoute)
+app.use('/api/orders', orderRoute)
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Backend server is running!')
